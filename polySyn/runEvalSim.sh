2111283eb2311fc872a65e9879e1c449f125dcb2
aetting <allyson.k.ettinger@gmail.com>
2015-10-26 11:07:14 -0400

    slight mod to sh

--- polySyn/runEvalSim.sh
+++ polySyn/runEvalSim.sh
@@ -4,6 +4,7 @@ sg_vecs='/Users/allysonettinger/Desktop/SenseRetrofit-master/mik-sg-80.txt'
 wordnet_sensevecs='/Users/allysonettinger/Desktop/SenseRetrofit-master/data/mik.sampont.sense'
 genformat='text'
 MENtest='/Users/allysonettinger/Desktop/similarity-datasets/MEN/MEN_dataset_lemma_form.test'
+MENdev='/Users/allysonettinger/Desktop/vectors/polySyn/similarity-datasets/MEN/MEN_dataset_lemma_form.dev'
 ws353_combined='/Users/allysonettinger/Desktop/similarity-datasets/wordsim353/ws353.combined.tab.formatted'
 
-python evaluateSim.py $sg_vecs $genformat $ws353_combined
+python evaluateSim.py $sg_vecs $genformat $MENdev
