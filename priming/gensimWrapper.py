9d535d9224730171fbec8345813c5a5593c537b2
aetting <allyson.k.ettinger@gmail.com>
2016-03-10 23:19:58 -0500

    update gensimWrapper

--- priming/gensimWrapper.py
+++ priming/gensimWrapper.py
@@ -1,4 +1,4 @@
-import gensim, os, sys, gzip
+import gensim, os, sys, gzip, re
 
 
 class MySent(object):
@@ -8,13 +8,15 @@ class MySent(object):
     def __iter__(self):
         for d in self.dirlist:
             for f in os.listdir(d):
-                for line in gzip.open(os.path.join(d,f)):
+                if f.endswith('.gz'): fileObject = gzip.open(os.path.join(d,f))
+                else: fileObject = open(os.path.join(d,f))
+                for line in fileObject:
                     if len(line.strip()) > 0 and not re.match('<\/doc>',line):
                         yield line.split()
 
 dirlist = [sys.argv[i] for i in range(2,len(sys.argv))]
 saveto = sys.argv[1]
 sentences = MySent(dirlist)
-for x in sentences: print x
-#model = gensim.models.Word2Vec(sentences,min_count=10)
-#model.save(saveto)
+#for x in sentences: print x
+model = gensim.models.Word2Vec(sentences,min_count=10)
+model.save(saveto)
